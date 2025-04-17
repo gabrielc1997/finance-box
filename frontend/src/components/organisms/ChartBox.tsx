8fd530aa3c7bf62320516ce422294f0bcaf98204
Gabriel Carvalho <gabrielc_1997@hotmail.com>
2025-04-16 23:59:35 -0300

    first commit

--- frontend/src/components/organisms/ChartBox.tsx
+++ frontend/src/components/organisms/ChartBox.tsx
@@ -1,18 +1,43 @@
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { ResponsiveLine } from '@nivo/line'
 
 export const ChartBox = () => {
   const data = [
-    { name: 'Jan', value: 400 },
-    { name: 'Feb', value: 300 },
-  ];
+    {
+      id: 'Valor',
+      color: 'hsl(251, 70%, 50%)',
+      data: [
+        { x: 'Jan', y: 400 },
+        { x: 'Feb', y: 300 },
+        { x: 'Mar', y: 100 },
+        { x: 'Apr', y: 300 },
+        { x: 'may', y: 1000 },
+        { x: 'jun', y: 500 },
+        { x: 'jul', y: 800 },
+        { x: 'ago', y: 2000 },
+        { x: 'set', y: 500 },
+        { x: 'out', y: 1000 },
+        { x: 'nov', y: 3000 },
+        { x: 'dez', y: 2000 }
+      ]
+    }
+  ]
 
   return (
-    <div className="p-4 bg-white rounded shadow-md">
-      <LineChart width={300} height={200} data={data}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Line type="monotone" dataKey="value" stroke="#8884d8" />
-      </LineChart>
+    <div className="p-4 bg-white rounded shadow-md" style={{ height: 300 }}>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 30, right: 30, bottom: 50, left: 40 }}
+        xScale={{ type: 'point' }}
+        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: false }}
+        axisBottom={{ legend: 'Mês', legendOffset: 36, legendPosition: 'middle' }}
+        axisLeft={{ legend: 'Valor', legendOffset: -30, legendPosition: 'middle' }}
+        colors={{ scheme: 'category10' }}
+        pointSize={8}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        useMesh={true}
+      />
     </div>
-  );
-};
+  )
+}
